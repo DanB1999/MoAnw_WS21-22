e0f671fca119a29120ffc59b611bcb860f6b9eae
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-10-29 12:36:17 +0200

    skipWaiting function

--- serviceWorker.js
+++ serviceWorker.js
@@ -31,7 +31,7 @@ self.addEventListener("fetch", fetchEvent => {
   );
 });
 
-addEventListener('activate', e => {
+self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => {
     return Promise.all(keys.map(key => {
       if (key != v) return caches.delete(key);
@@ -39,7 +39,7 @@ addEventListener('activate', e => {
   }));
 });
 
-addEventListener('message', e => {
+self.addEventListener('message', e => {
   if (e.data === 'skipWaiting') {
     skipWaiting();
   }
