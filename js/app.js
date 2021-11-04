5cac338236a9e9eb2bf4d456529d9aa15ae808bf
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-11-05 00:06:00 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -107,7 +107,7 @@ function takePhoto() {
       var theImageTag = document.getElementById("imageTag");
       theImageTag.src = URL.createObjectURL(blob);
       
-      console.log(theImageTag);
+      console.log(blob);
 
       imgData = getBase64Im(theImageTag);
       localStorage.setItem("picture", imgData); 
@@ -140,11 +140,12 @@ function getBase64Image(img) {
 }
 function getBase64Im(img) {
   var reader = new FileReader();
-  reader.readAsDataURL(img); 
   reader.onloadend = function() {
-  var base64data = reader.result;                
+  var base64data = reader.result;               
   console.log(base64data);
-}
+  }
+reader.readAsDataURL(img); 
+return base64data;
 }
 
 
