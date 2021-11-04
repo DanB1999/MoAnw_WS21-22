b6aa66b728679bc3a1264fef694343626fae168c
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-11-04 16:31:57 +0100

    base64

--- js/app.js
+++ js/app.js
@@ -112,6 +112,18 @@ function takePhoto() {
     })
     .catch(err => alert('Error: ' + err));
 }
+function getBase64Image(img) {
+  var canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+
+  var dataURL = canvas.toDataURL("image/png");
+
+  return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
 document.addEventListener("DOMContentLoaded", showCoffees);
 
 if ("serviceWorker" in navigator) {
