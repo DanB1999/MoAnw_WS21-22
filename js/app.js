e54eb44bb386ead47e12f9d0d39154930c38ed83
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-11-11 16:39:28 +0100

    gps

--- js/app.js
+++ js/app.js
@@ -141,6 +141,27 @@ function loadPicture()  {
   document.getElementById('tableBanner').src = dataImage;;
 
 }
+//Geoloction-Feature
+var target = document.getElementById('target');
+var watchId;
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+}
+
 
 //document.addEventListener("DOMContentLoaded", showPicture);
 
@@ -152,5 +173,4 @@ if ("serviceWorker" in navigator) {
       .catch(err => console.log("service worker not registered", err));
   });
 }
-
 
