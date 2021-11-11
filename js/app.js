29e0564891e43e8cff9105d70e6a1fec0e4cb6c3
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-11-11 17:04:09 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -107,9 +107,11 @@ function takePhoto() {
       var theImageTag = document.getElementById("imageTag");
       theImageTag.src = URL.createObjectURL(blob, {autorevoke : false });
       localStorage.setItem("ImageTag",theImageTag.src);
+      /*
       self.caches.open(staticDevCoffee).then(cache => {
         cache.addAll("ImageTag",theImageTag.src);
       })
+      */
       console.log(blob);
     })
     .catch(err => alert('Error: ' + err));
