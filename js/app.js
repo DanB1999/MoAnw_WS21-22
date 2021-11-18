10667818b619a2c7128ad811f68248fbc2910ad5
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-11-18 16:09:48 +0100

    update

--- js/app.js
+++ js/app.js
@@ -18,6 +18,144 @@ const showCoffees = () => {
   container.innerHTML = output;
 };
 */
+function getUserMedia(options, successCallback, failureCallback) {
+  var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  if (api) {
+    return api.bind(navigator)(options, successCallback, failureCallback);
+  }
+}
+
+var theStream;
+var theRecorder;
+var recordedChunks = [];
+
+function getStream(type) {
+  if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+    alert('User Media API not supported.');
+    return;
+  }
+  
+  var constraints = {};
+  constraints[type] = true;
+
+  getUserMedia(constraints, function (stream) {
+    var mediaControl = document.querySelector(type);
+    if ('srcObject' in mediaControl) {
+      mediaControl.srcObject = stream;
+    } else if (navigator.mozGetUserMedia) {
+      mediaControl.mozSrcObject = stream;
+    } else {
+      mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+    }
+    if(type=='video') {
+      theStream = stream;
+    }
+    mediaControl.play();
+  }, function (err) {
+    alert('Error: ' + err);
+  });
+}
+
+function takePhoto() {
+  if (!('ImageCapture' in window)) {
+    alert('ImageCapture is not available');
+    return;
+  }
+  
+  if (!theStream) {
+    alert('Grab the video stream first!');
+    return;
+  }
+  
+  var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+
+  theImageCapturer.takePhoto()
+    .then(blob => {
+      var theImageTag = document.getElementById("imageTag");
+      theImageTag.src = URL.createObjectURL(blob, {autorevoke : false });
+      
+      var reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onloadend = function() {
+        var base64data = reader.result;
+        console.log(base64data);
+        localStorage.setItem("ImageTag",base64data);
+      }
+    })
+    .catch(err => alert('Error: ' + err));
+}
+
+function loadPicture()  {
+  var base64 = localStorage.getItem('ImageTag');
+  document.getElementById('tableBanner').src = base64;;
+
+}
+/*
+function downloadImg()  {
+  var theImageTag = document.getElementById("imageTag");
+  var a = document.createElement(theImageTag.src);
+  a.download = 'picture.jpg';
+  a.href = window.URL.createObjectURL(theImageTag.src);
+  a.click();
+  }
+  */
+//Geoloction-Feature
+var target = document.getElementById('target');
+var watchId;
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+}
+//Device Position
+if ('DeviceOrientationEvent' in window) {
+  window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+} else {
+  document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
+}
+
+function deviceOrientationHandler (eventData) {
+  var tiltLR = eventData.gamma;
+  var tiltFB = eventData.beta;
+  var dir = eventData.alpha;
+  
+  document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+  document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+  document.getElementById("doDirection").innerHTML = Math.round(dir);
+
+  var logo = document.getElementById("imgLogo");
+  logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+  logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
+  logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+}
+
+//document.addEventListener("DOMContentLoaded", showPicture);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
+
+//############################Storage Quotas#############################
+
 if ('storage' in navigator && 'estimate' in navigator.storage) {
   navigator.storage.estimate()
     .then(estimate => {
