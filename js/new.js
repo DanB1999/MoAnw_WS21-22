4c6fe49f93d62d31c7e1aa10c8664b42c104e697
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-12-09 22:30:22 +0100

    update

--- js/new.js
+++ js/new.js
@@ -1,9 +1,41 @@
+class costObject {
+   constructor(desc, date, cat, amount, balance)    {
+       this.desc = desc;
+       this.date = date;
+       this.cat = cat;
+       this.amount = amount;
+       this.balance = balance;
+   }
+}
+
+const costObjects = new Array;
+const attributes = new Array;
+var balance = 0;
+var counter = 0;
+
 function embeddedElement(id)  {
+    var desc, date, cat, amount;
     var value = document.getElementById(id).value;
-    
     var x = document.getElementById(id).parentElement.id;
-    console.log(x);
     document.getElementById(x).innerHTML = value;
+    attributes.push(value);
+    if(value.typeof(Number)) balance -= value;
+    counter++;
+    if(counter = 0) desc = value;
+    else if(counter = 1) date = value;
+    else if(counter = 2) cat = value;
+    else if(counter = 3) amount = value;
+    else if(counter = 4)     {
+        document.getElementById("tdBalance").innerHTML = balance
+        costObjects.push(new costObject(desc, date, cat, amount, balance))
+    }
+}
+
+
+document.addEventListener("")
+
+function saveObject()   {
+
 }
 
 
