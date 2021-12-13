80faa24c4f5a2f8ff813a2a4d8fa708147b993bd
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-12-13 18:55:56 +0100

    Update new.js

--- js/new.js
+++ js/new.js
@@ -75,7 +75,7 @@ function getObjects()   {
     newRow();
 }
 
-
+/*
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
@@ -84,6 +84,19 @@ if ("serviceWorker" in navigator) {
         .catch(err => console.log("service worker not registered", err));
     });
 }
+*/
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
+      // registration worked
+      console.log('Registration succeeded.');
+      button.onclick = function() {
+        registration.update();
+      }
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  };
   
   
   
