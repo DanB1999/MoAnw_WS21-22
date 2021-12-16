9bdfde73dfbf4182daa80bab582e60a6949353f8
DanB1999 <83217388+DanB1999@users.noreply.github.com>
2021-12-16 17:07:04 +0100

    final

--- js/new.js
+++ js/new.js
@@ -77,7 +77,7 @@ function getObjects()   {
     newRow();
 }
 
-/*
+
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
@@ -86,67 +86,8 @@ if ("serviceWorker" in navigator) {
         .catch(err => console.log("service worker not registered", err));
     });
 }
-*/
 
 
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceWorker.js?v=' + version).then(function(registration) {
-      // registration worked
-      console.log('Registration succeeded.');
-      /*
-      document.getElementById("").onclick = function() {
-        registration.update();
-        console.log("Service Worker updated")
-      }
-      */
-    }).catch(function(error) {
-      // registration failed
-      console.log('Registration failed with ' + error);
-    });
-  };
-
-
-/*
-if (!navigator.serviceWorker.controller) {
-  console.log("This page is not controlled by a ServiceWorker");
-}
-else {
-  console.log("This page is controlled by a ServiceWorker");
-}
-
-navigator.serviceWorker.getRegistration().then(function(reg) {
-  function showWaitingMessage() {
-    console.log("A new ServiceWorker is waiting to become active. It can't become active now because pages are still open that are controlled by the older version. Either close those tabs, or shift+reload them (which loads them without the ServiceWorker). That will allow the new version to become active, so it'll be used for the next page load.");
-  }
-
-  reg.addEventListener('updatefound', function() {
-    console.log("Found a new ServiceWorker!");
-    var installing = reg.installing;
-    reg.installing.addEventListener('statechange', function() {
-      if (installing.state == 'installed') {
-        console.log("New ServiceWorker installed.");
-        // give it a second to see if it activates immediately
-        setTimeout(function() {
-          if (installing.state == 'activated') {
-            console.log("New ServiceWorker activated! Reload to load this page with the new ServiceWorker.");
-          }
-          else {
-            showWaitingMessage();
-          }
-        }, 1000);
-      }
-      else if (installing.state == 'redundant') {
-        console.log("The new worker failed to install - likely an error during install");
-      }
-    });
-  });
-
-  if (reg.waiting) {
-    showWaitingMessage();
-  }
-});
-*/
 
 
   
--- serviceWorker.js
+++ serviceWorker.js
@@ -7,47 +7,29 @@ const assets = [
 ];
 var number = 4; 
 
-self.addEventListener("install", installEvent => {
-  console.log("Installable");
-  installEvent.waitUntil(
-    caches
-    .open(staticDevCoffee)
-    .then(cache => {
-      cache.addAll(assets);
-    })
-    .catch(console.log(" Service Worker installed"))
-  );
-});
-
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
-  );
-});
-
 self.addEventListener('install', function(event) {
   // We pass a promise to event.waitUntil to signal how 
   // long install takes, and if it failed
+  console.log("Install Service Worker")
   event.waitUntil(
     // We open a cache…
-    caches.open('simple-sw-v1').then(function(cache) {
+    caches
+    .open('simple-sw-v1')
+    .then(function(cache) {
       // And add resources to it
       return cache.addAll([
         './',
         '/index.html',
         '/css/style.css',
         '/js/new.js'
-      ]);
+      ])
     })
   );
 });
-/*
-self.addEventListener('activate', async (event) => {
-
-  var cacheAllowlist = ['dev-coffee-site-v1'];
-
+self.addEventListener('installed', function(event)  {
+  console.log("Service Worker installed");
+  //Alten Cache löschen 
+  /*
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
@@ -60,9 +42,22 @@ self.addEventListener('activate', async (event) => {
       );
     })
   );
-  
+  */
+})
+
+self.addEventListener('activate', async (event) => {
+  console.log("Service Worker activate")
+  //neuen Cache schreiben
 });
-*/
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
+
 self.addEventListener('message', e => {
   if (e.data === 'skipWaiting') {
     skipWaiting();
@@ -70,7 +65,4 @@ self.addEventListener('message', e => {
   
 });
 
-function changeVersion() {
-  number++;
-}
 
